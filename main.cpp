d7c63bd21b2d75aa04970b69a6b0e77b932648bb
AlexONEX <a.schwartzmann@hotmail.com>
2023-06-26 22:14:08 -0300

    Problemas con header. Agregue resolver LU

--- main.cpp
+++ main.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
-#include <Eigen/Dense>
+#include <Eigen>
 #include "include/metodosIterativos/metodosIterativos.h"
+
 using namespace std;
 using namespace Eigen;
 
-
 int main() {   
-    MatrixXd matrix(3, 3);
-    matrix << 1, -0.5, 0.5,
-               1, 1, 1, 
-               -0.5, -0.5, 1;
-
+  
+    MatrixXd A(3, 3);
+    A << 10, -1, 2,
+         -1, 11, -1,
+         2, -1, 10;
+    
     VectorXd b(3);
     b << 4, -1, 1;           
     
     VectorXd x0(3); 
     x0 << 0, 0, 0;
-
+    
     int nIter = 50; 
 
-    VectorXd jacobiIter = jMatIter(matrix, b, x0, nIter);
-    VectorXd gsIter = gsMatIter(matrix, b, x0, nIter);
-    VectorXd expected = resolverLU(matrix, b);
-
+    VectorXd expected = resolverLU(A, b);
+    VectorXd jacobi = jMat(A, b, x0, nIter);
+    
+    cout << "Difference between expected and Jacobi method: " << (expected - jacobi).norm() << endl;
+    
     return 0; 
 }
