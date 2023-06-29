a21953c1eae983fd7401e6e2332654be4a17e7aa
AlexONEX <a.schwartzmann@hotmail.com>
2023-06-29 13:41:29 -0300

    Emprolige el main para testear tiempo y error

--- main.cpp
+++ main.cpp
@@ -1,29 +1,76 @@
 #include "include/metodosIterativos/metodosIterativos.h"
+#include <fstream>
+#include <chrono>
 
 using namespace std;
 using namespace Eigen;
 
-int main() {   
-  
-    MatrixXd A(3, 3);
-    A << 10, -1, 2,
-         -1, 11, -1,
-         2, -1, 10;
-    
-    VectorXd b(3);
-    b << 4, -1, 1;           
-    
-    VectorXd x0(3); 
-    x0 << 0, 0, 0;
-    
-    int nIter = 500; 
+//Setear semilla para replicar resultados
+void setSeed() {
+    srand(0);
+}
+
+// Función para generar una matriz aleatoria
+MatrixXd generateRandomMatrix(int size) {
+    MatrixXd matrix(size, size);
+    matrix.setRandom();
+    return matrix;
+}
+
+// Función para generar un vector aleatorio
+VectorXd generateRandomVector(int size) {
+    VectorXd vector(size);
+    vector.setRandom();
+    return vector;
+}
+
+// Función para escribir el resultado en un archivo de texto
+void writeResultToFile(string filename, string method, double time, double error) {
+    ofstream file(filename, ios::app);
+    if (file.is_open()) {
+        file << method << "," << time << "," << error << endl;
+        file.close();
+    } else {
+        cout << "Error al abrir el archivo: " << filename << endl;
+    }
+}
+
+int main() {
+    // Parámetros de prueba
+    int matrixSize = 100;
+    int numTests = 10;
+    int nIter = 500;
     double threshold = 0.00000000001;
     int checkeoNorma = 100;
 
-    VectorXd expected = resolverLU(A, b);
-    VectorXd aprox = jSum(A, b, x0, nIter, threshold, checkeoNorma);
-    
-    cout << "Diferencia entre Metodo Directo y Metodo Iterativo: " << (expected - aprox).norm() << endl;
+    // Generar matrices y vectores aleatorios y realizar las pruebas
+    for (int i = 0; i < numTests; i++) {
+        MatrixXd A = generateRandomMatrix(matrixSize);
+        VectorXd b = generateRandomVector(matrixSize);
+        VectorXd x0 = VectorXd::Zero(matrixSize);
 
-    return 0; 
+        auto start = chrono::high_resolution_clock::now();
+        VectorXd expected = A.lu().solve(b);
+        auto end = chrono::high_resolution_clock::now();
+        double luTime = chrono::duration_cast<chrono::microseconds>(end - start).count() * 1e-6;
+
+        start = chrono::high_resolution_clock::now();
+        VectorXd jsumResult = jSum(A, b, x0, nIter, threshold, checkeoNorma);
+        end = chrono::high_resolution_clock::now();
+        double jsumTime = chrono::duration_cast<chrono::microseconds>(end - start).count() * 1e-6;
+        double jsumError = (expected - jsumResult).norm();
+
+        start = chrono::high_resolution_clock::now();
+        VectorXd gsSumResult = gsSum(A, b, x0, nIter, threshold, checkeoNorma);
+        end = chrono::high_resolution_clock::now();
+        double gsSumTime = chrono::duration_cast<chrono::microseconds>(end - start).count() * 1e-6;
+        double gsSumError = (expected - gsSumResult).norm();
+
+        // Escribir los resultados en archivos
+        writeResultToFile("results.csv", "LU", luTime, 0.0);
+        writeResultToFile("results.csv", "jSum", jsumTime, jsumError);
+        writeResultToFile("results.csv", "gsSum", gsSumTime, gsSumError);
+    }
+
+    return 0;
 }
